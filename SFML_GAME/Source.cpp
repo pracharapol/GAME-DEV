c9433d473a44c4fd932b81dd43ecf88ece7f1ee7
PRACHARAPOL JARUVANAWAT <63010630@KMITL.AC.TH>
2020-10-20 01:59:21 +0700

    1

--- SFML_GAME/Source.cpp
+++ SFML_GAME/Source.cpp
@@ -3,15 +3,38 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1280, 820), "Bird of shit");
+	sf::RenderWindow window(sf::VideoMode(1390, 820), "Bird of shit");
+	////// Circle
+		sf::CircleShape collision1(50.f);
+		collision1.setPosition({ 60.f, 50.f });
+		collision1.setFillColor(sf::Color::Cyan);
+		
+	//background
+		sf::Texture background;
+		if (!background.loadFromFile("background/gg.png"))
+		{
+			std::cout << "Load failed" << std::endl;
+		}
 	
-	////// Texture
+	////// Texture player
 	sf::Texture playerTexture;
 	if (!playerTexture.loadFromFile("monster/kk.png"))
 	{
 		std::cout << "Load failed" << std::endl;
 	}
-
+	// rock
+	sf::Texture playerTexture1;
+	if (!playerTexture1.loadFromFile("hinder/rock.png"))
+	{
+		std::cout << "Load failed" << std::endl;
+	}
+	//rock2
+	sf::Texture playerTexture2;
+	if (!playerTexture2.loadFromFile("hinder/rock.png"))
+	{
+		std::cout << "Load failed" << std::endl;
+	}
+	
 	////// Sprite
 	sf::Sprite shapeSprite;
 	shapeSprite.setTexture(playerTexture);
@@ -24,41 +47,98 @@ int main()
 	shapeSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
 
 	int animationFrame = 0;
+	
+	//rock
+	sf::Sprite shapeSprite1;
+	shapeSprite1.setTexture(playerTexture1);
+	sf::Vector2f spawnPoint1 = { 1000.f, 350.f };
+	shapeSprite1.setPosition(spawnPoint1);
 
+	int spriteSizeX1 = playerTexture1.getSize().x / 8;
+	int spriteSizeY1 = playerTexture1.getSize().y / 8;
+
+	shapeSprite1.setTextureRect(sf::IntRect(0, 0, spriteSizeX1, spriteSizeY1));
+	
+	//rock2
+	sf::Sprite shapeSprite2;
+	shapeSprite2.setTexture(playerTexture2);
+	sf::Vector2f spawnPoint2 = { 900.f, 50.f };
+	shapeSprite2.setPosition(spawnPoint2);
+
+	int spriteSizeX2 = playerTexture2.getSize().x / 8;
+	int spriteSizeY2 = playerTexture2.getSize().y / 8;
+
+	shapeSprite2.setTextureRect(sf::IntRect(640, 0, spriteSizeX2, spriteSizeY2));
+	
+	//background
+	sf::Sprite backg;
+	backg.setTexture(background);
+	sf::Vector2f spawnPoint3 = { 0.f, 0.f };
+	backg.setPosition(spawnPoint3);
+
+	int spriteSizeX3 = background.getSize().x/1;
+	int spriteSizeY3 = background.getSize().y/1;
+
+	backg.setTextureRect(sf::IntRect(0, 0, spriteSizeX3, spriteSizeY3));
+
+	float totalTime = 0;
+	float speed = 400.0f;
+	float deltaTime = 0.0f;
+	sf::Clock clock;
 	while (window.isOpen())
 	{
+		window.draw(backg);
+		window.draw(collision1);
 		window.draw(shapeSprite);
+		window.draw(shapeSprite1);
+		window.draw(shapeSprite2);
 		window.display();
+		deltaTime = clock.restart().asSeconds();
+		
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
-			shapeSprite.move(.6f, 0.f);
+			shapeSprite.move(speed* deltaTime, speed*0.f);
 			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, 0, spriteSizeX, spriteSizeY));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 		{
-			shapeSprite.move(-.6f, 0.f);
+			shapeSprite.move(-speed * deltaTime, speed * 0.f);
 			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, 0, spriteSizeX, spriteSizeY));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 		{
-			shapeSprite.move(0.f, -.6f);
+			shapeSprite.move(speed * 0.f, -speed * deltaTime);
 			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, spriteSizeY));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 		{
-			shapeSprite.move(0.f, .6f);
+			shapeSprite.move(speed * 0.f, speed * deltaTime);
 			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, spriteSizeY));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 		{
 			window.close();
 		}
-		animationFrame++;
+		totalTime += deltaTime;
+		if (totalTime >= 0.1)
+		{
+			totalTime -= 0.1;
+			animationFrame++;
+		}
 
 		if (animationFrame >= 5) {
 			animationFrame = 0;
 		}
+		//rock
+		if (shapeSprite1.getGlobalBounds().intersects(shapeSprite.getGlobalBounds())) {
+			shapeSprite.setPosition({60.f,50.f});
+		}
+		//rock2
+		if (shapeSprite2.getGlobalBounds().intersects(shapeSprite.getGlobalBounds())) {
+			shapeSprite.setPosition({ 60.f,50.f });
+		}
 		window.clear();
-	}
+	}	
+
 	return 0;
 }
